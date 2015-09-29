97aadacf03c91929510a2b37ac13a77e024b7a2b
Alex Yang <gya10@sfu.ca>
2015-09-28 23:56:51 -0700

    update

--- JavaScript-TheGoodParts/program.js
+++ JavaScript-TheGoodParts/program.js
@@ -5,6 +5,10 @@ Function.prototype.method = function (name, func) {
 	return this;
 };
 
+var add = function(a, b){
+	return a + b;
+}
+
 //Do some testing
 var o1 = {
 	attr1 : "hello",
@@ -67,5 +71,75 @@ delete o1.attr1;
 document.writeln(another_o1.attr1);
 delete another_o1.attr1;
 document.writeln(another_o1.attr1);
+document.writeln();
 
 
+//The method invocation pattern
+var myObject = {
+	value: 0,
+	increment: function(inc){
+		this.value += typeof inc === 'number'? inc : 1;
+	},
+	getValue: function(){
+		return this.value;
+	}
+};
+document.writeln(myObject.value);
+myObject.increment();
+document.writeln(myObject.value);
+myObject.increment(3);
+document.writeln(myObject.value);
+document.writeln();
+
+
+//The function invocation pattern
+myObject.double = function(){
+	var that = this;
+	var helper = function(){
+		that.value = add(that.value, that.value);
+	};
+
+	helper(); //invoke helper as a function
+};
+myObject.double();
+document.writeln(myObject.getValue());
+document.writeln();
+
+
+//The constructor invocation pattern
+//Create a constructor function called Quo
+//It makes an object with a status proeprty
+var Quo = function(string){
+	this.status = string;
+};
+
+//Give all instances of Quo a public method
+//called get_status
+Quo.prototype.get_status = function(){
+	return this.status;
+}
+
+//make an instance of Quo
+var myQuo = new Quo("confused");
+document.writeln(myQuo.get_status());
+document.writeln();
+
+
+
+//***Importanta***
+//The apply invocation pattern
+var array = [3, 4];
+var sum = add.apply(null, array);
+
+//Make an object with a staus member.
+var statusObject = {
+	status : "OK"
+};
+
+//statusObject does not inherit from Quo.prototype,
+//but we can invoke the get_status method on
+//statusObject even though statusObject does not have
+//a get_status method.
+var status = Quo.prototype.get_status.apply(statusObject);
+document.writeln(status);
+document.writeln();
