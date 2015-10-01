fb6431d92a6b99559336686e88dfed2a5bc36c6a
Alex Yang <gya10@sfu.ca>
2015-09-30 23:00:26 -0700

    update

--- JavaScript-TheGoodParts/program.js
+++ JavaScript-TheGoodParts/program.js
@@ -1,8 +1,9 @@
 document.writeln('Hello World!');
 
 Function.prototype.method = function (name, func) {
-	this.prototype[name] = func;
-	return this;
+	if(!this.prototype[name]){
+		this.prototype[name] = func;
+	}
 };
 
 var add = function(a, b){
@@ -126,7 +127,7 @@ document.writeln();
 
 
 
-//***Importanta***
+//***Important***
 //The apply invocation pattern
 var array = [3, 4];
 var sum = add.apply(null, array);
@@ -143,3 +144,143 @@ var statusObject = {
 var status = Quo.prototype.get_status.apply(statusObject);
 document.writeln(status);
 document.writeln();
+
+
+//Arguments
+//Make a function that adds a lot of stuff
+var sum = function(){
+	var i, sum = 0;
+	for(i = 0; i < arguments.length; i++){
+		sum += arguments[i];
+	}
+	return sum;
+};
+document.writeln(sum(1,2,3,4,5));
+document.writeln();
+
+
+//Exception
+var add2 = function(a, b){
+	if (typeof a !== 'number' || typeof b !== 'number'){
+		throw {
+			name: 'TypeError',
+			message: 'Add function needs numbers'
+		};
+	}
+	return a + b;
+};
+
+var try_add2 = function(){
+	try{
+		add2('a', 'b');
+	}
+	catch(e){
+		//we can inpect the name of the exception to determine its type
+		document.writeln(e.name + ": "+e.message);
+	}
+};
+try_add2();
+document.writeln();
+
+
+//augmenting types
+//Add an integer method to Number.prototype which extract the integer part of a number
+Number.method('integer', function(){
+	return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
+document.writeln((-10 / 3).integer());
+
+//remove spaces from the ends of a string. It uses regular expression
+String.method('trim', function(){
+	return this.replace(/^\s+|\s+$/g, '');
+});
+document.writeln('"' + "    neat     ".trim() + '"');
+document.writeln();
+
+
+//Recursion
+var walk_the_DOM = function walk(node, func){
+	func(node);
+	node = node.firstChild;
+	while(node){
+		walk(node, func);
+		node = node.nextSibling;
+	}
+};
+
+var getElementsByAttribute = function (att, value){
+	var results = [];
+
+	walk_the_DOM(document.body, function (node){
+		var actual = node.nodeType === 1 && node.getAttribute(att);
+		if(typeof actual === 'string' && (actual === value || typeof value !== 'string')){
+			results.push(node);
+		}
+	});
+
+	return results;
+};
+getElementsByAttribute('alex','yang')[0].parentNode.onclick = function(){console.log("body adawd")};
+
+//tail recursion
+var factorial = function factorial(i, a){
+	a = a || 1;
+	if(i < 2){
+		return a;
+	}
+	return factorial(i-1, a*i);
+};
+document.writeln(factorial(4));
+document.writeln();
+
+
+
+//Scope:
+var foo = function(){
+	var a = 3, b = 5;
+
+	var bar = function(){
+		var b = 7, c = 11;
+		//at this point, a = 3, b= 7, c = 11
+		a+= b + c;
+		//at this point, a = 21, b = 7, c = 11
+	};
+
+	//at this point, a = 3, b = 5, c is not defined
+	bar();
+	//at this point, a = 21, b = 5, c = 11
+};
+
+
+//Closure: hiding information
+//we are not assiging a function to my2ndObject. We are assigning the 
+//result of invoking that function
+var my2ndObject = function(){
+	var value = 0;
+
+	return {
+		increment: function(inc){
+			value += typeof inc === 'number'? inc : 1;
+		},
+		getValue: function(){
+			return value;
+		}
+	};
+}();
+document.writeln(my2ndObject.getValue());
+my2ndObject.increment(5);
+document.writeln(my2ndObject.getValue());
+
+var Quo2 = function(status){
+	return {
+		get_status: function(){
+			return status;
+		}
+	};
+};
+var myQuo2 = Quo2("amazed");
+document.writeln(myQuo2.get_status());
+document.writeln();
+
+
