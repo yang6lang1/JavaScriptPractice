3995c5fcdc30584c7254be8b1461809a2e5b4115
Alex Yang <gya10@sfu.ca>
2015-10-07 21:52:28 -0700

    update

--- JavaScript-TheGoodParts/program.js
+++ JavaScript-TheGoodParts/program.js
@@ -325,6 +325,35 @@ var add_the_handlers = function(nodes){
 		}(i);
 	}
 };
-add_the_handlers([document.body]);
-
+//add_the_handlers([document.body]);
 document.writeln();
+
+
+//Module:
+//A module is a function or onject that presents an interface but hides
+//its state and implementation
+String.method('deentityify',function(){
+	//The entity table. It maps entity names to characters
+	var entity = {
+		quot: '"',
+		lt: '<',
+		gt: '>'
+	};
+
+	//return the deentityify method.
+	return function(){
+		//This is the deentityify method. It calls the string
+		//replace method, looking for substrings that start 
+		//with '&' and end with ';'. If the characters in
+		//between are in the entity table, then replace the
+		//entity with the character from the table. It uses
+		//a regular expression(Chapter 7)
+		return this.replace(/&([^&;]+);/g,
+			function(a,b){
+				var r = entity[b];
+				return typeof r === 'string'? r:a;
+			}
+		);
+	};
+}());
+document.writeln('&lt;&quot;&gt;'.deentityify());
