dbbe34af706a0d107e8603cea0e7f36841279f05
Alex Yang <gya10@sfu.ca>
2015-11-04 17:03:21 -0800

    Don't quite understand this section but it seems not that important. Will revisit in the future

--- JavaScript-TheGoodParts/program.js
+++ JavaScript-TheGoodParts/program.js
@@ -622,3 +622,63 @@ var coolcat = function(spec){
 };
 var myCoolCat = coolcat({name : 'Jinx'});
 document.writeln(myCoolCat.get_name());
+
+
+
+//parts
+//We can compose objects out of sets of parts
+var eventuality = function(that) {
+	var registry = {};
+
+	that.fire = function(event){
+		//fire an even on an object. The even can be either
+		//a string containing the name of the event or an
+		//object containing a type property containing the
+		//name of the event. Handlers registered by the 'on'
+		//method that match the even name will be invoked.
+		var array, func, handler, i,
+			type = typeof event === 'string' ?
+					event : event.type;
+		//If an array of handlers exist for this event, then
+		//loop through it and execute the handlers in order.
+		if(registry.hasOwnProperty(type)){
+			array = registry[type];
+			for(i = 0; i < array.length; i++){
+				handler = array[i];
+
+				//A handler record contains a method that an optional
+				//array of parameters. If the method is a name, look
+				//up the function
+				func = handler.method;
+				if(typeof func = 'string'){
+					func = this[func];
+				}
+
+				//Invoke a handler. If the record contained
+				//parameters, then pass them. Otherwise, pass the
+				//event object.
+				func.apply(this, handler.parameters || [event]);
+			}
+		}
+		return this;
+	};
+
+	that.on = function(type, method, parameters){
+		//Register an event. Make a handler record. Put it
+		//in a handler array, making one if it doesn't yet
+		//exist for this type.
+		var handler = {
+			method: method,
+			parameters: parameters
+		};
+		if(registry.hasOwnProperty(type)){
+			registry[type].push(handler);
+		}
+		else{
+			registry[type] = [handler];
+		}
+		return this;
+	};
+
+	return that;
+};
